a320a5a0610d5ff5c47e38354b21f2a5bdc47f7c
Camila <camilataglione@yahoo.com.br>
2023-10-13 12:09:07 -0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ICalcMedia calculoAritmetico = new Aritmetica();
+        ICalcMedia calculoGeometrico = new Geometrica();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Disciplina disciplinaAritmetica = new Disciplina(calculoAritmetico);
+        Disciplina disciplinaGeometrica = new Disciplina(calculoGeometrico);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        disciplinaAritmetica.setNome("Engenharia de Software");
+        disciplinaAritmetica.setP1(10);
+        disciplinaAritmetica.setP2(5);
+        disciplinaAritmetica.CalcularMedia();
+
+        disciplinaGeometrica.setNome("Sistemas Operacionais");
+        disciplinaGeometrica.setP1(9);
+        disciplinaGeometrica.setP2(8);
+        disciplinaGeometrica.CalcularMedia();
+
+        System.out.println("Disciplina Engenharia de Sortware:");
+        System.out.println(String.format("P1: %.2f P2: %.2f Média: %.2f Situação: %s",
+                disciplinaAritmetica.getP1(), disciplinaAritmetica.getP2(),
+                disciplinaAritmetica.getMedia(), disciplinaAritmetica.getSituacao()));
+
+        System.out.println("Disciplina Sistemas Operacionais:");
+        System.out.println(String.format("P1: %.2f P2: %.2f Média: %.2f Situação: %s",
+                disciplinaGeometrica.getP1(), disciplinaGeometrica.getP2(),
+                disciplinaGeometrica.getMedia(), disciplinaGeometrica.getSituacao()));
     }
 }
